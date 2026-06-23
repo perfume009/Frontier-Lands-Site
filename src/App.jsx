0af7573242ae81b508cb078b235160efaee1c28b
perfume009 <sstalker408@gmail.com>
2026-06-23 22:16:28 +0800

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -5,17 +6,34 @@ import Factions from './components/Factions'
 import ServerStatus from './components/ServerStatus'
 import Rules from './components/Rules'
 import Footer from './components/Footer'
+import Preloader from './components/Preloader'
 
-const App = () => (
-  <>
-    <Navbar />
-    <Hero />
-    <About />
-    <Factions />
-    <ServerStatus />
-    <Rules />
-    <Footer />
-  </>
-)
+const alreadyShown = sessionStorage.getItem('preloaderShown')
+
+const App = () => {
+  const [loading, setLoading] = useState(() => !alreadyShown)
+
+  useEffect(() => {
+    if (loading) {
+      sessionStorage.setItem('preloaderShown', '1')
+      const timer = setTimeout(() => setLoading(false), 1500)
+      return () => clearTimeout(timer)
+    }
+  }, [])
+
+  if (loading) return <Preloader />
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Factions />
+      <ServerStatus />
+      <Rules />
+      <Footer />
+    </>
+  )
+}
 
 export default App
---
+++ src/components/Preloader.jsx
@@ -0,0 +1,12 @@
+const Preloader = () => (
+  <div className="preloader">
+    <div className="preloader-inner">
+      <img src="/FLRP3.png" alt="Frontier Lands" className="preloader-logo" />
+      <div className="preloader-bar">
+        <div className="preloader-bar-fill" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Preloader;
